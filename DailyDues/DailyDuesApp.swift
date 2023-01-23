caf9e9d6cf7d5eeff9f131fc5145f3d2d4e73055
RMF <rmfdev1@gmail.com>
2023-01-23 13:27:36 -0500

    Setting up git repo

---
+++ DailyDues/DailyDuesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyDuesApp.swift
+//  DailyDues
+//
+//  Created by Ricardo Fernandez on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct DailyDuesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
